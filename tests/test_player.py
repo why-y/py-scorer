4d019dfacb7569cd3a276d6336a36d51343ef4a9
Yves Gross <yves@bond.info>
2023-11-28 17:17:09 +0100

    migrate to pytest starting with Player tests only

--- tests/test_player.py
+++ tests/test_player.py
@@ -1,18 +1,13 @@
-import unittest
+import pytest
 import scorer
 
 from scorer.player import Player
 
-class TestPlayer(unittest.TestCase):
+class TestPlayer:
 
     NAME = "Paul"
-
-    def setUp(self) -> None:
-        self.player = Player(TestPlayer.NAME)
+    player = Player(NAME)
 
     def test_init_name(self):
         initialName = self.player.name
-        self.assertEqual(initialName, TestPlayer.NAME)
-
-if __name__ == '__main__':
-    unittest.main()
+        assert initialName == self.NAME
